a18a4f0f6b3f624a069c82f76b816046624f7ddd
Geng Sun <gsun@iastate.edu>
2023-04-04 00:26:48 -0500

    Bind to directive properties

--- src/app/directive/high-lighter.directive.ts
+++ src/app/directive/high-lighter.directive.ts
@@ -4,30 +4,34 @@ import {
   Renderer2,
   OnInit,
   HostListener,
-  HostBinding
+  HostBinding,
+  Input
 } from '@angular/core';
 
 @Directive({
   selector: '[appHighLighter]'
 })
 export class HighLighterDirective implements OnInit {
+  @Input() defaultColor: string = 'transparent';
+  @Input() highlightColor: string = 'blue';
   // Bind to the host element's style.backgroundColor property
   @HostBinding('style.backgroundColor') backgroundColor: string;
 
   constructor(private elRef: ElementRef, private render2: Renderer2) { }
 
   ngOnInit() {
+    this.backgroundColor = this.defaultColor;
     //this.render2.setStyle(this.elRef.nativeElement, 'background-color', 'yellow');
   }
 
   //Listen to the host element's mouseenter event
   @HostListener('mouseenter', ['$event']) mouseover(eventData: Event) {
     // this.render2.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
-    this.backgroundColor = 'blue';
+    this.backgroundColor = this.highlightColor;
   }
 
   @HostListener('mouseleave', ['$event']) mouseleave(eventData: Event) {
     // this.render2.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
-    this.backgroundColor = 'transparent';
+    this.backgroundColor = this.defaultColor;
   }
 }
